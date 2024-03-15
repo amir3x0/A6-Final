c6d44b872e00e70060c311d17ed1540fd1d0abe3
Amir Mishayev <127884315+amir3x0@users.noreply.github.com>
2024-03-15 19:31:13 +0200

    imported immgakit Co-authored-by: litalleschinsky <litalleschinsky@users.noreply.github.com>

--- frontend/src/components/ProfileSection.js
+++ frontend/src/components/ProfileSection.js
@@ -2,7 +2,18 @@ import React, { useState, useEffect } from "react";
 import { useUser } from "../context/UserContext";
 import { fetchRecipeById } from "../services/BackendService";
 import RecipeCard from "../components/RecipeCard";
+import { IKContext, IKImage, IKUpload } from "imagekitio-react";
 import Settings from "./settings"; // Adjust the import path as necessary
+
+async function fetchAvatarPics() {
+  return [
+    "/avatar_pics/avatar1.jpg",
+    "/avatar_pics/avatar2.jpg",
+    "/avatar_pics/avatar3.jpg",
+    "/avatar_pics/avatar4.jpg",
+    "/avatar_pics/avatar5.jpg",
+  ];
+}
 
 export default function MyYummy() {
   const { user } = useUser(); // Access user data from context
@@ -12,6 +23,25 @@ export default function MyYummy() {
   const [uploadedRecipes, setUploadedRecipes] = useState([]);
   const [showSettings, setShowSettings] = useState(false);
   const [showOptions, setShowOptions] = useState(false);
+  const [avatarPics, setAvatarPics] = useState([]);
+
+  useEffect(() => {
+    if (user.favoriteRecipes && user.favoriteRecipes.length > 0) {
+      fetchRecipes(user.favoriteRecipes[0], setFavoriteRecipes); // Assuming favoriteRecipes is an array with a single string of IDs
+    }
+    if (user.uploadedRecipes && user.uploadedRecipes.length > 0) {
+      fetchRecipes(user.uploadedRecipes[0], setUploadedRecipes); // Assuming uploadedRecipes is an array with a single string of IDs
+    }
+  }, [user]);
+
+  useEffect(() => {
+    const getAvatars = async () => {
+      const avatars = await fetchAvatarPics();
+      setAvatarPics(avatars);
+    };
+
+    getAvatars();
+  }, []);
 
   const handleRecipeClick = (id) =>
     setExpandedRecipeId(expandedRecipeId === id ? null : id);
@@ -33,14 +63,7 @@ export default function MyYummy() {
     setter(recipes);
   };
 
-  useEffect(() => {
-    if (user.favoriteRecipes && user.favoriteRecipes.length > 0) {
-      fetchRecipes(user.favoriteRecipes[0], setFavoriteRecipes); // Assuming favoriteRecipes is an array with a single string of IDs
-    }
-    if (user.uploadedRecipes && user.uploadedRecipes.length > 0) {
-      fetchRecipes(user.uploadedRecipes[0], setUploadedRecipes); // Assuming uploadedRecipes is an array with a single string of IDs
-    }
-  }, [user]);
+
 
   // Adjust this to correctly render recipe cards based on user data
   const renderRecipeCards = (recipes) =>
@@ -75,6 +98,8 @@ export default function MyYummy() {
     return <div>Loading user data...</div>; // Or handle user not found more gracefully
   }
 
+
+
   const toggleOptions = () => {
     setShowOptions(!showOptions);
   };
@@ -94,107 +119,109 @@ export default function MyYummy() {
   };
 
   return (
-    <div className="container mx-auto px-5">
-      <div className="flex flex-wrap -mb-4">
-        {/* Profile and settings block */}
-        <div className="flex flex-col items-center w-1/3 p-6 bg-white rounded-lg shadow-xl relative">
-          <img
-            src={user.profileImageUrl || "default_profile_image_url"}
-            alt="Profile"
-            className="rounded-full h-48 w-48 object-cover shadow-lg border-4 border-blue-300 cursor-pointer"
-            onClick={toggleOptions}
-          />
-          {showOptions && (
-            <div className="options-overlay absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center bg-black bg-opacity-50">
-              <button
-                className="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={handleUpload}
-              >
-                Upload
-              </button>
-              <button
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                onClick={handleChooseAvatar}
-              >
-                Choose Avatar
-              </button>
-            </div>
-          )}
-          <h2 className="text-3xl font-extrabold text-center mt-4 text-blue-600">
-            {user.name}
-          </h2>
-          <p className="text-base text-center text-gray-500 mt-2">
-            @{user.username}
-          </p>
-          <p className="text-center mt-4 text-lg text-gray-700">
-            {user.bio || "No bio available"}
-          </p>
-          <button
-            className="mt-6 bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full transition-colors duration-150 ease-in-out"
-            onClick={toggleSettings}
-          >
-            Settings
-          </button>
-        </div>
-
-        {/* Favorite and uploaded recipes, and meal plans */}
-        <div className="w-full lg:w-2/3 px-4">
-          {/* Favorite Recipes Section */}
-          <div className="mb-8">
-            <h2 className="text-4xl font-extrabold text-indigo-600 tracking-tight">
-              Favorite Recipes
-            </h2>
-            {favoriteRecipes.length > 0 ? (
-              <div
-                className="grid grid-cols-3 gap-6 overflow-auto"
-                style={{ maxHeight: "calc(100px * 6)" }}
-              >
-                {renderRecipeCards(favoriteRecipes)}
+    <IKContext
+      publicKey="your_public_api_key"
+      urlEndpoint="https://ik.imagekit.io/k0hnty7yv"
+      transformationPosition="path"
+      authenticationEndpoint="http://www.yourserver.com/auth"
+    >
+      <div className="container mx-auto px-5">
+        <div className="flex flex-wrap -mb-4">
+          <div className="flex flex-col items-center w-1/3 p-6 bg-white rounded-lg shadow-xl relative">
+            <img
+              src={user.profileImageUrl || "default_profile_image_url"}
+              alt="Profile"
+              className="rounded-full h-48 w-48 object-cover shadow-lg border-4 border-blue-300 cursor-pointer"
+              onClick={toggleOptions}
+            />
+            {showOptions && (
+              <div className="options-overlay absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center bg-black bg-opacity-50">
+                <IKUpload fileName="my-upload" />
+                {avatarPics.map((avatarPath, index) => (
+                  <IKImage
+                    key={index}
+                    path={avatarPath}
+                    transformation={[{ height: "100", width: "100" }]}
+                    onClick={() => console.log(`Avatar ${index} selected`)}
+                  />
+                ))}
               </div>
-            ) : (
-              <p>No favorite recipes added yet.</p>
             )}
-          </div>
-
-          {/* Uploaded Recipes Section */}
-          <div className="mb-8">
-            <h2 className="text-4xl font-extrabold text-indigo-600 tracking-tight">
-              Uploaded Recipes
+            <h2 className="text-3xl font-extrabold text-center mt-4 text-blue-600">
+              {user.name}
             </h2>
-            {uploadedRecipes.length > 0 ? (
-              <div
-                className="grid grid-cols-3 gap-6 overflow-auto"
-                style={{ maxHeight: "calc(100px * 6)" }}
-              >
-                {renderRecipeCards(uploadedRecipes)}
-              </div>
-            ) : (
-              <p>No recipes uploaded yet.</p>
-            )}
+            <p className="text-base text-center text-gray-500 mt-2">
+              @{user.username}
+            </p>
+            <p className="text-center mt-4 text-lg text-gray-700">
+              {user.bio || "No bio available"}
+            </p>
+            <button
+              className="mt-6 bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full transition-colors duration-150 ease-in-out"
+              onClick={toggleSettings}
+            >
+              Settings
+            </button>
           </div>
-
-          <div className="mb-8">
-            <h2 className="text-4xl font-extrabold text-indigo-600 tracking-tight">
-              Meal Plans
-            </h2>
-            {user.MealPlans && user.MealPlans.length > 0 ? (
-              user.MealPlans.map((plan) => (
-                <div key={plan.id} className="py-2">
-                  {plan.title}
+          {/* Favorite and uploaded recipes, and meal plans */}
+          <div className="w-full lg:w-2/3 px-4">
+            {/* Favorite Recipes Section */}
+            <div className="mb-8">
+              <h2 className="text-4xl font-extrabold text-indigo-600 tracking-tight">
+                Favorite Recipes
+              </h2>
+              {favoriteRecipes.length > 0 ? (
+                <div
+                  className="grid grid-cols-3 gap-6 overflow-auto"
+                  style={{ maxHeight: "calc(100px * 6)" }}
+                >
+                  {renderRecipeCards(favoriteRecipes)}
                 </div>
-              ))
-            ) : (
-              <p>No meal plans added yet.</p>
-            )}
-          </div>
-        </div>
+              ) : (
+                <p>No favorite recipes added yet.</p>
+              )}
+            </div>
 
-        {/* Debugging: Display user object
+            {/* Uploaded Recipes Section */}
+            <div className="mb-8">
+              <h2 className="text-4xl font-extrabold text-indigo-600 tracking-tight">
+                Uploaded Recipes
+              </h2>
+              {uploadedRecipes.length > 0 ? (
+                <div
+                  className="grid grid-cols-3 gap-6 overflow-auto"
+                  style={{ maxHeight: "calc(100px * 6)" }}
+                >
+                  {renderRecipeCards(uploadedRecipes)}
+                </div>
+              ) : (
+                <p>No recipes uploaded yet.</p>
+              )}
+            </div>
+
+            <div className="mb-8">
+              <h2 className="text-4xl font-extrabold text-indigo-600 tracking-tight">
+                Meal Plans
+              </h2>
+              {user.MealPlans && user.MealPlans.length > 0 ? (
+                user.MealPlans.map((plan) => (
+                  <div key={plan.id} className="py-2">
+                    {plan.title}
+                  </div>
+                ))
+              ) : (
+                <p>No meal plans added yet.</p>
+              )}
+            </div>
+          </div>
+
+          {/* Debugging: Display user object
         <div>
           <h3>User Object:</h3>
           <pre>{JSON.stringify(user, null, 2)}</pre>
         </div> */}
+        </div>
       </div>
-    </div>
+    </IKContext>
   );
 }
