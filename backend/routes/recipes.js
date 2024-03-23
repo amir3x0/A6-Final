c9b385201ae71725eeb848a03598a031f2f8b3b1
IPblood <118939745+IPblood@users.noreply.github.com>
2024-03-23 05:53:27 +0200

    share almost work

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,27 +1,27 @@
 const express = require('express')
 const {
-    getRecipe,
-    getRecipes,
-    createRecipe,
-    deleteRecipe,
-    updateRecipe,
-} = require('../controllers/recipeController')
+  getRecipe,
+  getRecipes,
+  shareRecipe,
+  deleteRecipe,
+  updateRecipe,
+} = require("../controllers/recipeController");
 
 const router = express.Router()
 
-//Get all recipes
+// Get all recipes
 router.get('/', getRecipes)
 
-//Get single recipe
+// Get single recipe
 router.get('/findone/:id', getRecipe)
 
-//Post a new recipe
-router.post('/',createRecipe)
+// Post a new recipe
+router.post("/share", shareRecipe);
 
-//Update a recipe 
+// Update a recipe 
 router.patch('/:id',updateRecipe)
 
-//Delete a recipe 
+// Delete a recipe 
 router.delete('/:id', deleteRecipe)
 
 
